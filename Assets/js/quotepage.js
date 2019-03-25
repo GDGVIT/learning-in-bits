e13fe707938b9d2d01f243fbc1553bf421970a72
Ananya <ananyaganesh01@gmail.com>
2019-03-25 22:05:00 +0530

    Added functionality in tags

--- Assets/js/quotepage.js
+++ Assets/js/quotepage.js
@@ -1,7 +1,15 @@
+
 $("input[type='text']").keypress(function(e){
     if(e.which === 13){
         var tag = $(this).val();
         $(this).val("");
-        $
+        $("#two").append('<div class="col-4 col-sm-2"><div id="uno">'+tag+'<span><i class= "fa fa-times"></i></div></div></span>');
+
     }
-})
+});
+$("#two").on("click", "span" , function(e){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
