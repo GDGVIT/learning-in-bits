68c0778a2bf683e035624c944cfda1798ce9a9d2
Ananya <ananyaganesh01@gmail.com>
2019-03-27 21:52:33 +0530

    Seperate signup and login

---
+++ profile.js
@@ -0,0 +1,9 @@
+// $(".name").click(function(){
+//     alert("clciked!");
+//     $(".edit").css('visibility','visible');
+// });
+$(".name").click(function(){
+    $(this).css('display','none');
+    $(".edit").css('display','inline');
+
+});
