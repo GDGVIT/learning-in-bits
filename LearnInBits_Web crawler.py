1369bf4776c1c90c0b9441a51ecdbf68a99e0bc3
Riddhi <mail2riddhigupta@gmail.com>
2019-03-29 14:51:47 +0530

    Improved it's working

--- LearnInBits_Web crawler.py
+++ LearnInBits_Web crawler.py
@@ -10,7 +10,6 @@ def quoteScraper():
     allLinks=[]
     allTags=[]
     allQoutes=[]
-
     client=MongoClient('mongodb+srv://RiddhiGupta_05:LearningBits@learning-in-bits-xoruo.mongodb.net/test?retryWrites=true')
     db=client["Learn-in-Bits-database"]
     collection=db['Quotes']
@@ -28,13 +27,15 @@ def quoteScraper():
         if(link!=None and re.search('/topic_index/',link)):
             link='https://www.brainyquote.com'+link
             allLinks.append(link)
-            print(link)
+            
     
+
     for link in allLinks:
         try:
             driver.get(link)
             sauce=driver.page_source
             soup=bs.BeautifulSoup(sauce,'lxml')
+            
         except :
             print("*************************************")
             print(link)
@@ -46,14 +47,16 @@ def quoteScraper():
             if(l!=None and (l not in allTags) and re.search('/topics/',l)):
                 l='https://www.brainyquote.com'+l
                 allTags.append(l)
-                print(l)
+                
         
     
+
     for link in allTags:
         driver.get(link)
         sauce=driver.page_source
         soup=bs.BeautifulSoup(sauce,'lxml')
         body=soup.find('body')
+        Quotes=[]
         Boxes=body.find_all('div', class_='qll-bg')
         for b in Boxes:
             l=b.find('a')
@@ -61,36 +64,24 @@ def quoteScraper():
             if(l!=None and re.search('/quotes/',l)):
                 l='https://www.brainyquote.com'+l
                 allQoutes.append(l)
-                print(l)
-        
-
-    while(len(allQoutes)!=0):
-
-        quote=allQoutes[0]
-        driver.get(quote)
-        sauce=driver.page_source
-        soup=bs.BeautifulSoup(sauce,'lxml')
-        body=soup.find('body')
-        
-        q=body.find_all('div', class_='quoteContent')
-        q=q[0].find('p')
-        q=q.text
-        tag=body.find_all('div', class_="bqQt bq_s")
-        tag=tag[0].find_all('a')
-        t=[]
-        for temp in tag:
-            t.append(temp.text)
-        element={'Tags':t,"Quote":q}
-        print(element)
-        posts.insert_one(element)
-        Boxes=body.find_all('div', class_='qll-bg')
-        for b in Boxes:
-            l=b.find('a')
-            l=l.get('href')
-            if(l!=None and (l not in allQoutes) and re.search('/quotes/',l)):
-                l='https://www.brainyquote.com'+l
-                allQoutes.append(l)
-        del allQoutes[0]
+                Quotes.append(l)
+        for quote in Quotes:
+            driver.get(quote)
+            sauce=driver.page_source
+            soup=bs.BeautifulSoup(sauce,'lxml')
+            body=soup.find('body')
+                    
+            q=body.find_all('div', class_='quoteContent')
+            q=q[0].find('p')
+            q=q.text
+            tag=body.find_all('div', class_="bqQt bq_s")
+            tag=tag[0].find_all('a')
+            t=[]
+            for temp in tag:
+                t.append(temp.text)
+            element={'Tags':t,"Quote":q}
+            #print(element)
+            posts.insert_one(element)
     
 
 quoteScraper()
